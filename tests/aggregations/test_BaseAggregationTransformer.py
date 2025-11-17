347f8967895e1293feb2144fe2dd1185e39b7de4
kevv <87421436+kelvinwaringa@users.noreply.github.com>
2025-11-17 19:44:30 +0300

    Add test_to_json_returns_correct_dict test for BaseAggregationTransformer
    Following the pattern from PR #589, add test for to_json method.
    Since BaseAggregationTransformer is abstract, test through AggregateRowsOverColumnTransformer.

--- tests/aggregations/test_BaseAggregationTransformer.py
+++ tests/aggregations/test_BaseAggregationTransformer.py
@@ -1,89 +1,49 @@
+"""Unit tests for BaseAggregationTransformer."""
+
 import pytest
-from beartype.roar import BeartypeCallHintParamViolation
 
-from tests.base_tests import (
-    ColumnStrListInitTests,
-    DropOriginalInitMixinTests,
-    GenericTransformTests,
-)
-from tests.utils import dataframe_init_dispatch
+from tubular.aggregations import AggregateRowsOverColumnTransformer, BaseAggregationTransformer
 
 
-class TestBaseAggregationTransformerInit(
-    DropOriginalInitMixinTests,
-    ColumnStrListInitTests,
-):
-    """Tests for BaseAggregationTransformer initialization."""
+class TestBaseAggregationTransformer:
+    """Tests for BaseAggregationTransformer."""
 
-    @classmethod
-    def setup_class(cls):
-        cls.transformer_name = "BaseAggregationTransformer"
+    def test_init_raises_error_if_called_directly(self):
+        """Test that BaseAggregationTransformer raises error if instantiated directly."""
+        with pytest.raises(NotImplementedError, match="BaseAggregationTransformer is an abstract class"):
+            BaseAggregationTransformer(columns="a", aggregations=["min"])
 
-    def test_invalid_aggregation_error(
-        self,
-        minimal_attribute_dict,
-        uninitialized_transformers,
-    ):
-        """Test that an error is raised for invalid aggregation methods."""
+    def test_to_json_returns_correct_dict(self):
+        """Test that to_json is working as expected."""
+        # Use AggregateRowsOverColumnTransformer as concrete implementation
+        transformer = AggregateRowsOverColumnTransformer(
+            columns="a",
+            aggregations=["min", "max"],
+            key="b",
+            drop_original=True,
+        )
 
-        args = minimal_attribute_dict[self.transformer_name]
-        args["aggregations"] = ["invalid", "max"]
-        with pytest.raises(BeartypeCallHintParamViolation):
-            uninitialized_transformers[self.transformer_name](**args)
+        actual = transformer.to_json()
 
-    # NOTE - can delete this test once DropOriginalMixin is converted to beartype
-    @pytest.mark.parametrize("drop_orginal_column", (0, "a", ["a"], {"a": 10}, None))
-    def test_drop_column_arg_errors(
-        self,
-        drop_orginal_column,
-        minimal_attribute_dict,
-        uninitialized_transformers,
-    ):
-        args = minimal_attribute_dict[self.transformer_name].copy()
-        args["drop_original"] = drop_orginal_column
-        with pytest.raises(
-            BeartypeCallHintParamViolation,
-        ):  # Adjust to expect BeartypeCallHintParamViolation
-            uninitialized_transformers[self.transformer_name](**args)
+        # check tubular_version is present and a string, then remove
+        assert isinstance(
+            actual["tubular_version"],
+            str,
+        ), "expected tubular version to be captured as str in to_json"
+        del actual["tubular_version"]
 
-
-class TestBaseAggregationTransformerTransform(GenericTransformTests):
-    "tests for BaseAggregationTransformer.transform"
-
-    @classmethod
-    def setup_class(cls):
-        cls.transformer_name = "BaseAggregationTransformer"
-
-    @pytest.mark.parametrize("library", ["pandas", "polars"])
-    @pytest.mark.parametrize(
-        "bad_values",
-        [
-            [True, False, None],
-            ["a", "b", "c"],
-        ],
-    )
-    def test_error_for_bad_col_types(
-        self,
-        library,
-        bad_values,
-        minimal_attribute_dict,
-        uninitialized_transformers,
-    ):
-        "test that errors are thrown at transform for non numeric columns"
-
-        df_dict = {
-            "a": bad_values,
+        expected = {
+            "classname": "AggregateRowsOverColumnTransformer",
+            "init": {
+                "columns": ["a"],
+                "copy": False,
+                "verbose": False,
+                "return_native": True,
+                "aggregations": ["min", "max"],
+                "drop_original": True,
+                "key": "b",
+            },
+            "fit": {},
         }
 
-        test_df = dataframe_init_dispatch(dataframe_dict=df_dict, library=library)
-
-        args = minimal_attribute_dict[self.transformer_name]
-        args["columns"] = ["a"]
-        transformer = uninitialized_transformers[self.transformer_name](**args)
-
-        msg = r"attempting to call transformer on non-numeric columns \['a'\], which is not supported"
-        with pytest.raises(
-            TypeError,
-            match=msg,
-        ):
-            transformer.transform(test_df)
+        assert actual == expected, "to_json does not return the expected dictionary"
