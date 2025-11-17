36374d50f41aacfc4532da20f9459a34f71b58ea
liamholmes31 <liamholmes1996@gmail.com>
2025-11-17 12:08:31 +0000

    linting

--- feature_table.py
+++ feature_table.py
@@ -1,3 +1,5 @@
+"""Script to run in pre-commit/CI in order to keep the README feature table up to date."""
+
 import pathlib
 import re
 
@@ -9,7 +11,13 @@ ROOT = pathlib.Path(__file__).resolve().parents[1]
 
 
 def get_feature_table() -> str:
-    """Function to process FEATURE_REGISTRY into markdown table to README"""
+    """Process FEATURE_REGISTRY into markdown table to README.
+
+    Returns
+    -------
+        str: markdown table
+
+    """
     df = pd.DataFrame.from_dict(FEATURE_REGISTRY, orient="index").sort_index()
     # replace bools with unicode tick/cross
     df = df.replace({True: ":heavy_check_mark:", False: ":x:"})
@@ -18,7 +26,13 @@ def get_feature_table() -> str:
 
 
 def insert_table_to_readme(table: str, readme_text: str) -> None:
-    """Function to insert markdown table into repo README file"""
+    """Insert markdown table into repo README file.
+
+    Returns
+    -------
+        str: updated markdown text
+
+    """
     START = "<!-- AUTO-GENERATED feature table -->"
     END = "<!-- /AUTO-GENERATED feature table -->"
     pattern = re.compile(rf"{re.escape(START)}.*?{re.escape(END)}", flags=re.DOTALL)
@@ -37,4 +51,4 @@ if __name__ == "__main__":
     updated_readme_text = insert_table_to_readme(table, readme_text)
 
     if readme_text != updated_readme_text:
-        readme_path.write_text(updated_readme_text)
+        readme_path.write_text(updated_readme_text, encoding="utf8")
