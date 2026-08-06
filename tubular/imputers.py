26194567d2d0aa2ccaac7d41f1cc2251c893a577
Liam Holmes <liamholmes1996@gmail.com>
2026-08-06 14:36:50 +0000

    updated numberimputer error message

--- tubular/imputers.py
+++ tubular/imputers.py
@@ -376,7 +376,7 @@ class NumberImputer(BaseImputer):
 
         if bad_types:
             msg = f"""
-                ArbitraryImputer: transformer can only handle Float/Int/UInt/Unknown type columns
+                {self.classname()}: transformer can only handle Float/Int/UInt/Unknown type columns
                 but got columns with types {bad_types}
                 """
             raise TypeError(
